799dda7ea9dae2e2846af686f8d02448908601e0
Madeline Conway <conway.maddy@yahoo.com>
2024-08-01 13:42:26 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const name = ["Charlie", "Samip", "Ali"];
+
+const newArray = []
+
+function writeCards(name) {
+  for (let i = 0; i < name.length; i++) {
+    debugger;
+    newArray.push (`Thank you, ${name[i]}, for the wonderful surprise gift!`) 
+ console.log (newArray)
+}
+
+  return newArray;
+}
+
+
+
+
+function countDown(n) {
+while (n >= 0){
+console.log (n--)}
+}
+
+
+countDown (11)
